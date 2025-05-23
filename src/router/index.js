bfc3b63e4d0f77bebd855a57dd8b0fe4227305fc
PhanVinh09 <phanthevinh09w25@gmail.com>
2025-05-23 20:06:49 +0700

    add scrollBehavior to scroll to top on route change

--- src/router/index.js
+++ src/router/index.js
@@ -6,17 +6,23 @@ import EffectsAnimate from '@/components/effects/EffectsAnimate.vue'
 import EffectsScroll from '@/components/effects/EffectsScroll.vue'
 import Effects3D from '@/components/effects/Effects3D.vue'
 
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: Home,},
+    { path: '/', component: Home },
     { path: '/about', component: About },
-    { path: '/effect/hover', component: EffectsHover},
-    { path: '/effect/animate', component: EffectsAnimate},
-    { path: '/effect/scroll', component: EffectsScroll},
-    { path: '/effect/3d', component: Effects3D},
+    { path: '/effect/hover', component: EffectsHover },
+    { path: '/effect/animate', component: EffectsAnimate },
+    { path: '/effect/scroll', component: EffectsScroll },
+    { path: '/effect/3d', component: Effects3D },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition 
+    } else {
+      return { top: 0 } 
+    }
+  }
 })
 
 export default router
