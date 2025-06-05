dcf5aad836456faac5332881d08073491e2c406a
PhanVinh09 <phanthevinh09w25@gmail.com>
2025-06-05 18:22:40 +0700

    Added Model and Controller for Effect, UI, Layout and Auth

---
+++ laravel-api/app/Http/Controllers/UseInterfaceController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UseInterface;
+use Illuminate\Http\Request;
+
+class UseInterfaceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UseInterface $useInterface)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UseInterface $useInterface)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, UseInterface $useInterface)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UseInterface $useInterface)
+    {
+        //
+    }
+}
