0f2c53c1b6fa66a28b7d99f3e404f966c9500f18
PhanVinh09 <phanthevinh09w25@gmail.com>
2025-06-14 14:27:21 +0700

    Add edit feafure

--- laravel-api/app/Http/Controllers/FormController.php
+++ laravel-api/app/Http/Controllers/FormController.php
@@ -43,12 +43,38 @@ class FormController extends Controller
             'js.max' => 'Js tối đa là 60000 ký tự',
         ]);
         Form::create($validated);
-        return redirect()->route('forms.index')->with('success', 'Thêm hiệu ứng thành công');
+        return redirect()->route('forms.index')->with('success', 'Thêm form thành công');
     }
 
     public function update(Request $request, $id)
     {
-        //
+        $validated = $request->validate([
+            'author' => 'required|string|max:100',
+            'form_name' => 'required|string|max:100',
+            'type' => 'required|string|max:100',
+            'title' => 'nullable|string|max:255',
+            'link' => 'nullable|string|max:60000',
+            'html' => 'nullable|string|max:60000',
+            'css' => 'nullable|string|max:60000',
+            'js' => 'nullable|string|max:60000'
+        ], [
+            'author.max' => 'Author tối đa là 100 ký tự',
+            'form_name.required' => 'Form_name không được để trống',
+            'form_name.max' => 'Form_name tối đa là 100 ký tự',
+            'type.required' => 'Type không được để trống',
+            'type.max' => 'Type tối đa là 100 ký tự',
+            'title.max' => 'title tối đa là 255 ký tự',
+            'author.required' => 'Author tối đa là 100 ký tự',
+            'author.required' => 'Author tối đa là 100 ký tự',
+            'author.required' => 'Author tối đa là 100 ký tự',
+            'link.max' => 'Link tối đa là 60000 ký tự',
+            'html.max' => 'HTML tối đa là 60000 ký tự',
+            'css.max' => 'CSS tối đa là 60000 ký tự',
+            'js.max' => 'Js tối đa là 60000 ký tự',
+        ]);
+        $forms = Form::findOrFail($id);
+        $forms->update($validated);
+        return redirect()->route('forms.index')->with('success', 'Cập nhật form thành công');
     }
 
     public function destroy($id)
